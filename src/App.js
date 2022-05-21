ac5f3fb87c94b7f32a721952680bc44d800c9d78
andonyn19 <andonynunez2@gmail.com>
2022-05-20 23:19:00 -0500

    Update secondary files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import './App.css';
+
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import Featured from './components/Featured';
 
 function App() {
   return (
-    <div className="App">
-      1
-    </div>
+    <>
+      <NavBar />
+      <Hero />
+      <Featured />
+    </>
   );
 }
 
